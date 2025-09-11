70243dcc642230e7ad245e2b979c696cd8faafc7
redspidersoftware <ochentainueve@gmail.com>
2025-09-11 09:11:05 +0200

    Primera versión del mapa

---
+++ src/components/Mapa.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+// Fix iconos
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
+  iconUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+});
+
+export default function Mapa() {
+  return (
+    <MapContainer
+      center={[40.4168, -3.7038]} // Madrid
+      zoom={6}
+      style={{ height: "100vh", width: "100%" }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/">OSM</a>'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[40.4168, -3.7038]}>
+        <Popup>🚨 Alerta en Madrid</Popup>
+      </Marker>
+    </MapContainer>
+  );
+}
