9ecf4de1667d7d1d8314a7ca513b6e2dce3ffc00
redspidersoftware <ochentainueve@gmail.com>
2025-09-11 15:54:19 +0200

    mensaje del cambio

---
+++ src/lib/supabaseClient.ts
@@ -0,0 +1,7 @@
+// Archivo totalmente estático, export correcto
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
